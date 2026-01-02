f17dc8964e7473550a3bae70ec1ac1437b84cbf2
m-sipho <241426803+m-sipho@users.noreply.github.com>
2026-01-02 18:21:01 +0200

    Add components required for the whole project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { Navbar } from "@/layout/Navbar"
+import { Hero } from "@/sections/Hero";
+import { Projects } from "@/sections/Projects"
+import { Education } from "@/sections/Education"
+import { About } from "@/sections/About"
+import { Contact } from "@/sections/Contact"
+
 function App() {
-  return <></>
+  return <div className="min-h-screen overflow-x-hidden">
+    <Navbar />
+    <main>
+      <Hero />
+      <Projects />
+      <Education />
+      <About />
+      <Contact />
+    </main>
+  </div>
 }
 
 export default App
